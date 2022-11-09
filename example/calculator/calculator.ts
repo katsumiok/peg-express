48bcdddf63e3a4aaacc2425a52cd548c90020dbc
ok-da <aduko.reverse@gmail.com>
2022-11-09 20:37:47 +0900

    Update example code

--- example/calculator/calculator.ts
+++ example/calculator/calculator.ts
@@ -1,12 +1,12 @@
 import { Expression, Factor, Parser, Term, NodeTerminal } from './parser';
 
 export class Calculator extends Parser {
-  override Factor([number, parenthesizedExpression]: Factor) {
-    if (number) {
-      return number.value;
-    }
-    const [, expr] = parenthesizedExpression;
-    return expr.value;
+  override Expression([term, rest]: Expression) {
+    return rest.reduce(
+      (acc, [[add], term]): number =>
+        add ? acc + term.value : acc - term.value,
+      term.value
+    );
   }
 
   override Term([factor, rest]: Term) {
@@ -17,15 +17,15 @@ export class Calculator extends Parser {
     );
   }
 
-  override Expression([term, rest]: Expression) {
-    return rest.reduce(
-      (acc, [[add], term]): number =>
-        add ? acc + term.value : acc - term.value,
-      term.value
-    );
+  override Factor([number, parenthesizedExpression]: Factor) {
+    if (number) {
+      return number.value;
+    }
+    const [, expr] = parenthesizedExpression;
+    return expr.value;
   }
 
   override Number(node: NodeTerminal) {
-    return parseInt(node.text);
+    return parseInt(node.text, 10);
   }
 }
